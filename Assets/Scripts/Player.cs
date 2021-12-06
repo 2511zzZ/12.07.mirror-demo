bd386d0b390efdf585940a7725187144b682e79f
2511zzZ <2511672322@qq.com>
2021-12-07 03:03:54 +0800

    multiplay demo

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+public class Player : NetworkBehaviour
+{
+    void HandlerMovement()
+    {
+        if (isLocalPlayer)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+            Vector3 movement = new Vector3(moveHorizontal * 0.1f, moveVertical * 0.1f, 0);
+            transform.position = transform.position + movement;
+        }
+    }
+
+    private void Update()
+    {
+        HandlerMovement();
+    }
+}
